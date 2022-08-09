29d6decca6e5530bb8db8e870b05e5bde7fcff68
Patrick StarCode <90501975+patrick7star@users.noreply.github.com>
2022-08-08 21:13:32 -0300

    o processo de revestir tabelas está quase completo

--- src/tabelas.py
+++ src/tabelas.py
@@ -6,6 +6,7 @@
 
 from os import get_terminal_size
 from copy import deepcopy
+from array import array as Array
 
 class Coluna():
    def __init__(self, rotulo, array):
@@ -195,6 +196,217 @@ def otimiza_uso_de_tela(linhas):
    ...
 ...
 
+from texto import MatrizTexto
+# cantos da tabela.
+(CSE, CSD, CID, CIE) = ('\u256D', '\u256E', '\u256F', '\u2570')
+(BARRA_V, BARRA_H) = ('\u2502','\u2500')
+BARRA_CRUZ = '\u253c'
+
+
+class TabelaStrMatriz(MatrizTexto):
+   def __init__(self, tabela_str):
+      largura = tabela_str.find('\n') + 1
+      altura =  sum(
+         1 if char == '\n' else 0
+         for char in tabela_str
+      ) 
+      altura = len(tabela_str.split('\n'))
+      super().__init__(altura, largura-1)
+      k = 0
+      for (y, linha) in enumerate(tabela_str.split('\n')):
+         for (x, char) in enumerate(linha):
+            self.altera(y % altura, x % largura, char)
+         ...
+      ...
+   ...
+
+   @staticmethod
+   def to_matriz(tabela_str):
+      # só aceita strings.
+      if type(tabela_str) != str:
+         raise TypeError()
+
+      for y in range(altura):
+         for x in range(largura):
+            char = tabela_str[k]
+            if char != '\n':
+               matriz[y][x] = char
+            k += 1
+         ...
+      ...
+      return (matriz, (altura, largura))
+   ...
+
+   def __repr__(self):
+      caracteres = []
+      for linha in self._linhas:
+         for char in linha:
+            caracteres.append(char)
+         caracteres.append('\n')
+      ...
+      return "".join(caracteres)
+   ...
+...
+
+
+# acha colunas e linhas que são homogêneas,
+# na verdade, apenas parecidas com a barra.
+# Retorna um iterador com as posições de
+# colunas.
+def acha_colunas(ts):
+   (altura, largura) = ts.dimensao()
+   lista_de_posicoes = []
+
+   for x in range(1, largura-1):
+      for y in range(1, altura-1):
+         e_barra_vertical = (
+            ts[y][x] == BARRA and 
+            ts[y][x-1] == MARGEM and
+            ts[y][x+1] == MARGEM 
+         )
+         if e_barra_vertical:
+            info = (y, x, BARRA_V)
+            lista_de_posicoes.append(info)
+         ...
+      ...
+   ...
+   # caso especias da primeira e última borda.
+   for y in range(1, altura-1):
+      limite_esquerdo = (
+         ts[y][0] == BARRA and
+         ts[y][1] == MARGEM
+      )
+      limite_direito = (
+         ts[y][largura-1] == BARRA and
+         ts[y][largura-2].isspace()
+      )
+      if limite_esquerdo: 
+         lista_de_posicoes.append((y, 0, BARRA_V))
+
+      if limite_direito:
+         lista_de_posicoes.append((y, largura-1, BARRA_V))
+   ...
+
+   return lista_de_posicoes 
+...
+
+def acha_linhas(tabela_str):
+   (altura, largura) = tabela_str.dimensao()
+   # primeira as colunas
+   percentual = 0.0
+   for y in range(altura):
+      for x in range(largura):
+         char = tabela_str[y][x]
+         if char == BARRA:
+            percentual += 1.0/float(largura)
+      else:
+         if 0.7 <= percentual  <= 1:
+            yield(y)
+      ...
+      percentual = 0
+   ...
+...
+
+def acha_cruzilhadas(ts):
+   (altura, largura) = ts.dimensao()
+   lista_de_posicoes = []
+   for x in range(1,largura-1):
+      for y in range(1, altura-1):
+         uma_cruz_valida = (
+            ts[y-1][x] == ts[y+1][x] 
+                  ==
+            ts[y][x+1] == ts[y][x-1]
+                  == 
+                BARRA 
+         ) 
+         if uma_cruz_valida:
+            lista_de_posicoes.append((y, x, BARRA_CRUZ))
+      ...
+   ...
+   return lista_de_posicoes
+...
+
+def acha_cantos(ts):
+   (altura, largura) = ts.dimensao()
+   # lista de posições.
+   lista = []
+
+   # vai do segundo caractére ao penúltimo
+   # já que os cantos são adicionados sem
+   # qualquer iteração.
+   for x in range(1, largura-2):
+      L_invertido_esquerda = (
+         ts[0][x] == '#' and
+         ts[0][x-1] == '#' and
+         ts[1][x] and 
+         ts[0][x + 1] == ESPACO
+      )
+
+      L_invertido_direita = (
+         ts[0][x] == '#' and
+         ts[1][x] == '#' and
+         ts[0][x+1] == '#' and
+         ts[0][x-1] == ESPACO
+      )
+
+      L_direita = (
+         ts[altura-1][x] == '#' and
+         ts[altura-1][x+1] == '#' and
+         ts[altura-2][x] == '#' and
+         ts[altura-1][x-1] == ESPACO
+      )
+
+      L_esquerda = (
+         ts[altura-1][x] == '#' and
+         ts[altura-1][x-1] == '#' and
+         ts[altura-2][x] == '#' and
+         ts[altura-1][x+1] == ESPACO
+      )
+
+      # a posição e o caractére que têm
+      # que trocar.
+      if L_invertido_direita:
+         lista.append((0, x, CSE))
+      elif L_invertido_esquerda:
+         lista.append((0, x, CSD))
+
+      if L_direita:
+         lista.append((altura-1, x, CIE))
+      elif L_esquerda:
+         lista.append((altura-1, x, CID))
+   ...
+
+   # inserções de todos cantos no 
+   # sentido horário.
+   lista.append((0, 0, CSE))
+   lista.append((0, largura-1, CSD))
+   lista.append((altura-1, largura-1, CID))
+   lista.append((altura-1, 0, CIE))
+   return lista
+...
+
+def reveste(tabela_str):
+   if type(tabela_str) != TabelaStrMatriz:
+      raise TypeError()
+
+   posicoes = [
+      acha_cantos(tabela_str),
+      acha_colunas(tabela_str),
+      acha_cruzilhadas(tabela_str)
+   ]
+
+   for iterador in posicoes:
+      esta_vazio = len(iterador) == 0
+      while (not esta_vazio):
+         (y, x, char) = iterador.pop()
+         tabela_str.altera(y, x, char)
+         # atualizando valor lógico ...
+         esta_vazio = len(iterador) == 0
+      ...
+   ...
+   return str(tabela_str)
+...
+
 
 # testes unitários:
 if __name__ == "__main__":
@@ -229,7 +441,19 @@ if __name__ == "__main__":
       imprime(esboco_inicial)
    ...
 
+   def testa_funcao_reveste():
+      rol = list(choice(['F', 'M']) for _ in range(132))
+      coluna1 = Coluna("gênero", rol)
+      rol = list(randint(5, 100) for _ in range(129))
+      coluna2 = Coluna("idade", rol)
+      esboco_inicial = aglomera_colunas(coluna1, coluna2)
+      otimiza_uso_de_tela(esboco_inicial)
+      esboco = "\n".join(esboco_inicial)
+      print(reveste(TabelaStrMatriz(esboco)))
+   ...
+
    executa_teste(testa_funcao_cria_tabela)
    executa_teste(teste_de_funcao_tampa_tabela)
    executa_teste(testa_funcao_ct_dobradura)
+   executa_teste(testa_funcao_reveste)
 ...
