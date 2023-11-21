a2c4f691e375e29253eae11df774db667ec53475
Patrick StarCode <90501975+patrick7star@users.noreply.github.com>
2023-11-21 13:31:55 -0300

    estrutura `Ponto` aperfeiçoada.

--- src/tela.py
+++ src/tela.py
@@ -7,14 +7,21 @@ de reutilização de tal biblioteca é o máximo possível.
 # bibliotecas:
 import os, sys, copy
 
+# determinando o que será importado.
+__all__ = ["Tela", "Ponto"]
+
+from typing import TypeVar, Tuple
+# tipo genérico para valor interno ao ponto, podem ser decimais ou
+# inteiros simples.
+T = TypeVar("T", int, float)
 
 class Ponto:
+   __slot__ = ["lin", "col"]
    """
-   objeto tipo coordenada para cuidar bem dos
-   pontos do programa, ao decorrer do programa,
-   muitos são utilizados.
+   objeto tipo coordenada para cuidar bem dos pontos do programa, ao
+   decorrer do programa, muitos são utilizados.
    """
-   def __init__(self, y, x):
+   def __init__(self, y: T, x: T) -> None:
       # só aceita valores positivos.
       if y < 0 or x < 0:
          raise ArithmeticError("valor negativo no par \"(y, x)\"")
@@ -23,57 +30,26 @@ class Ponto:
          self.col = x
       pass
 
-   def distancia(self, ponto):
+   def distancia(self, p) -> T:
       " cálcula a distância entre dois pontos "
       # parâmetro tem que ser do tipo ponto.
-      if type(ponto) != type(Ponto):
+      if isinstance(ponto, Ponto):
          raise TypeError("argumento do tipo errado! tem que ser 'Ponto'")
       # varições verticais e horizontais.
-      dy2 = (ponto.lin - self.lin)**2
-      dx2 = (ponto.col - self.col)**2
+      dy2 = (p.lin - self.lin)**2
+      dx2 = (p.col - self.col)**2
       # fórmula para computar distância.
       return sqrt(dy2 + dx2)
+   ...
 
-   def __eq__(self, ponto):
+   def __eq__(self, q):
       " verifica se ponto passado é igual a instância "
-      return (ponto.lin == self.lin and
-              ponto.col == self.col)
+      return (q.lin == self.lin and q.col == self.col)
 
    def __ne__(self, ponto):
       " verifica se o argumento(um ponto) é diferente da instância "
       # parte do presuposto que ambos não são iguais.
       return not (self ==  ponto)
-
-   def __gt__(self, ponto):
-      """
-      verifica se a instância está no canto superior
-      esquerdo da tela, em relação ao ponto passado
-      como argumento. """
-      # eles tem que inicialmente, não serem o
-      # mesmo, ou seja, não estarem na mesma posição.
-      p1 = self != ponto
-      # não são aceitos mesmos 'y' ou 'x'
-      # dos pontos.
-      p2 = self.lin < ponto.lin
-      p3 = self.col < ponto.col
-
-      # as três posições tem que ser
-      # verídicas.
-      return p1 and p2 and p3
-
-   def __lt__(self, ponto):
-      """
-      verifica se a instância não está, no canto
-      superior esquerdo, relativo ao ponto passado
-      como argumento. """
-      # eles tem que inicialmente, não serem o
-      # mesmo, ou seja, não estarem na mesma posição.
-      p = self != ponto
-      # o resto é a negação do método anterior, se ele
-      # no caso identifica o ponto superior esquerdo da 
-      # relação, este aqui, reconhece quem não é 
-      # tal ponto.
-      return (self > ponto) and p
 
    def __xor__(self, ponto):
       # eles não podem está na mesma coluna...
@@ -85,8 +61,139 @@ class Ponto:
    ...
    def __sizeof__(self):
       return sys.getsizeof(self.x) + sys.getsizeof(self.y)
+...
+
+from collections.abc import Iterator
+# tupla com quatro coordenadas, sendo elas colineares entre sí, no
+# objetivo de formar um retângulo. Eles estão distribuídos do começo,
+# sendo o ponto superior esquerdo ao inferior direito, baseado numa
+# movimentação no sentido anti-horário.
+Retangulo = Tuple[Ponto, Ponto, Ponto, Ponto]
+
+from enum import (Enum, auto)
+# adicionando nos métodos e artificios ao classe acima.
+class Ponto(Ponto):
+   __slot__ = ["iteracao_terminada", "ordenada"]
+
+   def colinear(self, a: Ponto) -> bool:
+      "não podem está sob uma mesma linha vertical ou horizontal."
+      return a.y == self.y or self.x == a.x
+
+   @staticmethod
+   def _ajusta_pontos(p: Ponto, q: Ponto) -> (Ponto, Ponto):
+      """
+      retorna tupla com ponto inferior à esquerda de superior à direita.
+      Mas o que quer dizer direita e esquerda aqui? Bem, se o ponto está
+      contido -- e não se pode está adjascente a ele -- no retângulo
+      formado pelas projeções do ponto mais distante, então ele é menor,
+      assim dever está à esquerda, e o ponto da projeção do retângulo,
+      à direita.
+      """
+      (xP, yP, xQ, yQ) = (p.x, p.y, q.x, q.y)
+      if (xP > xQ and yP > yQ):
+         # inverte a ordem dos argumentos dado na tupla.
+         return (q, p)
+      else:
+         # apenas retorna a ordem anterior.
+         return (p, q)
    ...
 
+   # o arranjo certo de pontos.
+   class Disposicao(Enum):
+      # pontos, respectivamnete, superior-esquerdo e inferior-direito.
+      SEID = auto()
+      # pontos superior-direito e inferior-esquerdo, nesta ordem.
+      SDIE = auto()
+   ...
+
+   @staticmethod
+   def _modo(p: Ponto, q: Ponto) -> Disposicao:
+      (xP, yP, xQ, yQ) = (p.x, p.y, q.x, q.y)
+
+      if (xP < xQ) and (yP < yQ):
+         return Ponto.Disposicao.SEID
+      elif (yP > yQ) and (xP < xQ):
+         return Ponto.Disposicao.SDIE
+      else:
+         raise ValueError("pontos não são válidos(diagonais)!")
+   ...
+
+   @staticmethod
+   def retangulo(p: Ponto, q: Ponto) -> Retangulo:
+      """
+      dado dois pontos "não-colienares"(quero dizer que não tem
+      coordenadas coincidentes), ele retorna uma tupla com quatro
+      pontos, entre eles estes, representando as vértices do retângulo
+      que os pontos dados formam. Eles seguirão o seguinte padrão:
+      do ponto superior-esquerdo sendo o primeiro, e o inferior-esquerdo
+      o último, arranjados no sentido horário.
+      """
+      # não aceita pontos "colineares", ou seja, numa mesma reta.
+      if p.colinear(q):
+         raise ValueError(
+            "pontos com coordenadas correspodente,"
+            + " não são válidos."
+         )
+      ...
+
+      (p, q) = Ponto._ajusta_pontos(p, q)
+      # vendo se os pontos estão distribuídos na forma superior-esquerdo
+      # e inferior-direito, ou, superior-direito e inferior esquerdo.
+      match Ponto._modo(p, q):
+         case Ponto.Disposicao.SEID:
+            # superior-direito:
+            b = Ponto(p.y, q.x)
+            # inferior-esquerdo:
+            d = Ponto(q.y, p.x)
+            # superior-esquerdo e inferior-direito, respectivamente:
+            a = p; c = q;
+         case Ponto.Disposicao.SDIE:
+            # superior-esquerdo:
+            a = Ponto(p.y, q.x)
+            # inferior-direito:
+            c = Ponto(q.y, p.x)
+            # superior-direito e inferior-esquerdo:
+            b = p; d = q;
+         case _:
+            raise Exception("nunca chega até aqui")
+      ...
+      return (a, b, c, d)
+   ...
+
+   # funções de acessos das duas coordenadas.
+   def valor_abssissa(self):
+      return self.col
+   def valor_ordenada(self):
+      return self.lin
+
+   # acesso via coordenadas padrões.
+   x = property(valor_abssissa, None, None, None)
+   y = property(valor_ordenada, None, None, None)
+
+   # modo de formar lista ou tuplas com este tipo.
+   def __iter__(self) -> Iterator[T]:
+      self.ordenada = False
+      self.iteracao_terminada = False
+      return self
+   ...
+   def __next__(self) -> T:
+      if (not self.ordenada):
+         self.ordenada = True
+         return self.lin
+      elif self.iteracao_terminada:
+         raise StopIteration("ambos y e x já foram iterados!")
+      else:
+         self.iteracao_terminada = True
+         return self.col
+   ...
+
+   # modo de visualizar a estrutura de dados.
+   def __str__(self) -> str:
+      return "Ponto: y={} e x={}".format(self.lin, self.col)
+
+   def __repr__(self) -> str:
+      return "({}, {})".format(self.y, self.x)
+...
 
 class Tela:
    """
@@ -108,7 +215,7 @@ class Tela:
       # ou igual a dimensão do terminal.
       # A segunda linha é o número de linhas.
       if C > dimensao.columns or C <= 20:
-         self.colunas = dimensao.columns 
+         self.colunas = dimensao.columns
       else:
          self.colunas = C-1
 
@@ -401,7 +508,7 @@ class Tela:
          # marcar todos coordenadas, ou estrutura delas
          # marcada na chamada desta função:
          #coords = []
-         # verifica se o ponto 'A' está no canto 
+         # verifica se o ponto 'A' está no canto
          # nos cantos da Tela:
          canto_superior_esquerdo = A == Ponto(0, 0)
          canto_superior_direito = (B.col == self.colunas and A.lin == 0)
@@ -472,13 +579,56 @@ class Tela:
       acumulado = sum(sys.getsizeof(l) for l in self.matriz)
       return (
          acumulado +
-         sys.getsizeof(self.matriz) + 
+         sys.getsizeof(self.matriz) +
          sys.getsizeof(self.linhas) +
          sys.getsizeof(self.colunas) +
          sys.getsizeof(self.realizacoes)
       )
    ...
+...
 
-# determinando o que será importado.
-__all__ = ["Tela", "Ponto"]
+import unittest
+
+class ClassePonto(unittest.TestCase):
+   @unittest.skip("método 'retângulo'ainda não terminado.")
+   def resultado_de_retangulo(self):
+      p = Ponto(1, 5)
+      q = Ponto(6, 9)
+      resultado = Ponto.retangulo(p, q)
+      saida = (p, Ponto(1, 9), q, Ponto(6, 5))
+      self.assertEqual(resultado, saida)
+      print("resultado:", resultado)
+      print("saída:", saida)
+
+      # teste de outro:
+      a = Ponto(5, 5)
+      b = Ponto(9, 3)
+      resultado = Ponto.retangulo(a, b)
+      saida = (Ponto(5, 3), a, Ponto(9, 5), b)
+      self.assertEqual(resultado, saida)
+      print("resultado:", resultado)
+      print("saída:", saida)
+   ...
+   def novo_modo_de_acessar_atributos(self):
+      a = Ponto(5, 23); b = Ponto(10, 3)
+      print(str(a), str(b))
+      print("a ---> x={} e y={}".format(a.x, a.y))
+      print("b ---> x={} e y={}".format(b.x, b.y))
+   ...
+   def testando_iteracao(self):
+      pontos = [ Ponto(15, 30), Ponto(3, 10), Ponto(7, 16) ]
+      saidas = [(15, 30), (3, 10), (7, 16)]
+
+      for (i, p) in enumerate(pontos):
+         novo_p = tuple(p)
+         novo_novo_p = list(p)
+         print(novo_p, novo_novo_p)
+         self.assertEqual(novo_p, saidas[i])
+      ...
+   ...
+...
+
+if __name__ == "__main__":
+   unittest.main()
+
 
