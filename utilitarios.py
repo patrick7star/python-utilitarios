46317801e0958310ddf9ed0d45835d8e207cbbe2
Charles Montgomery Burns <90501975+patrick7star@users.noreply.github.com>
2025-05-18 20:25:33 -0300

    listagem é feita de maneira automática agora

--- utilitarios.py
+++ utilitarios.py
@@ -28,26 +28,34 @@ __all__ = [
 from sys import path
 from os import getenv, system, remove
 from os.path import join, abspath, dirname, exists
-import enum
+import enum, sys
 from pathlib import Path
 
-# Importando...
+# Tentando importar...
 #import barra_de_progresso(descontinuado)
-import src.progresso as progresso
-import src.impressao as impressao
-import src.espiral as espiral
-import src.legivel as legivel
-import src.romanos as romanos
-# Sendo renomeada com a versão otimizada, pelo menos até o momento.
-import src.tela as tela
-import src.arvore as arvore
-import src.extenso as extenso
-import src.impressao as impressao
-#import src.tabelas as tabelas
-import src.texto as texto
-import src.tempo as tempo
-# Não usado muito, então dado como descontinuado.
-import src.aritimetica as aritimetica
+try:
+   import src.progresso    as progresso
+   import src.impressao    as impressao
+   import src.espiral      as espiral
+   import src.legivel      as legivel
+   import src.romanos      as romanos
+   # Sendo renomeada com a versão otimizada, pelo menos até o momento.
+   import src.tela         as tela
+   import src.arvore       as arvore
+   import src.extenso      as extenso
+   import src.impressao    as impressao
+   #import src.tabelas as tabelas
+   import src.texto        as texto
+   import src.tempo        as tempo
+   # Não usado muito, então dado como descontinuado.
+   import src.aritimetica  as aritimetica
+except ModuleNotFoundError:
+   print("Provavelmente o módulo não foi encontrado por que isso é uma " +
+      "biblioteca externa, e pra reduzir a cadeia de árquivos ele foi " +
+      "bastante reduzido, ao menos aqueles arquivos(módulos) que não " + 
+      "tem depedencia de cadeia"
+   )
+
 
 # Computa o tipo de dado que é o objeto passado, por exemplo: Classe, 
 # Função, Enum, Variável e etc. Retorna uma string informando o tipo.
@@ -101,14 +109,44 @@ def listagem_do_modulo(modulo):
    print("")
 ...
 
+def todos_modulos_importados_manualmente() -> list:
+   """
+   Extrai todos módulos manuais importados aqui. Assim não será preciso
+   listar as bibliotecas que foram importadas acima.
+   """
+   # A extração de tais módulos será automática.
+   return list(map(
+      # Apenas extrai o módulo, o nome pode ser obtido novamente.
+      lambda tupla: sys.modules[tupla[1]],
+      filter(
+        # A filtragem aqui leva em consideração se há um segundo
+        # sub-diretório, se houver então descarta. Aqui será apenas 
+        # considerado os módulos em 'src'.
+        lambda tupla: len(tupla[1].split('.')) == 2,
+        filter(
+          # Filtra baseado se tem o nome 'src' nele.
+          lambda tupla: "src" in tupla[1],
+          # Guarda numa tupla o módulo e seu nome em string, respectivamente.
+          map(lambda mod: (mod, str(mod)), sys.modules)
+   ))))
+
+
 if __name__ == "__main__":
+   MODULOS_EXTRAIDOS = todos_modulos_importados_manualmente()
+
+   if __debug__:
+      print("\nO que será visualizado ...")
+      for modulo in MODULOS_EXTRAIDOS: 
+         print("\t\b\b-", modulo.__name__)
+
    print("\nImportando módulos ...\n")
 
-   todos_modulos = [
-      progresso, legivel, espiral, arvore, romanos, impressao, extenso, 
-      aritimetica, tempo
-   ]
-
-   for modulo in todos_modulos:
-      listagem_do_modulo(modulo)
+   for In in MODULOS_EXTRAIDOS:
+      try:
+         listagem_do_modulo(In)
+      except AttributeError:
+         print(
+            "__all__ não foi definido para %s. Talvez não sejá uma "
+            "biblioteca válida." % In.__name__, end="\n\n"
+         )
 ...
